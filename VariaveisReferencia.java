28159cfe456adfaacc3020bb88315e3d2a1b9911
Cris Mai <crismai.dev@gmail.com>
2022-05-10 15:29:10 -0300

    Variáveis de referência - Garbage Collector

---
+++ VariaveisReferencia.java
@@ -0,0 +1,22 @@
+// Apresentar as variáveis de referência
+
+
+public class VariaveisReferencia {
+	
+	public static void main (String [] args) {
+		
+		//Variável de referência
+		String y = "XTI";
+				
+	}
+	
+}
+
+/** Variável de referência
+É toda aquela que aponta para um objeto;
+*/
+
+/** Garbage Collector
+Gerencia os objetos que naõ tem mais referência com nenhuma variável em 
+seu programa, liberando assim memória em seu aplicativo;
+*/
