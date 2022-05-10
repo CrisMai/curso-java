a0a593cc6857ebf4ff65edf88e3da85c47db3289
Cris Mai <crismai.dev@gmail.com>
2022-05-10 15:16:03 -0300

    Tipos primitivos de variáveis

---
+++ TiposPrimitivos.java
@@ -0,0 +1,24 @@
+
+// Apresentar os tipos primitivos de variáveis
+
+
+public class TiposPrimitivos {
+	
+	public static void main (String [] args) {
+		
+		int idade = 36;
+		double preco = 12.45;
+		char sexo = 'F';
+		boolean casada = true;
+				
+	}
+	
+}
+
+/** Tipos de variáveis
+
+int byte short
+long float double
+char void boolean
+
+*/
