485cd12affab62886bfc4d3a28873e53732b0bba
Cris Mai <crismai.dev@gmail.com>
2022-05-10 15:51:04 -0300

    Constante e modificador final

---
+++ Constante.java
@@ -0,0 +1,26 @@
+// Apresentar as constantes e modificador final
+
+
+public class Constante {
+	
+	public static void main (String [] args) {
+		
+		final double PI = 3.141592;
+				
+	}
+	
+}
+
+/** Constante
+Valor nunca se altera;
+Escrever o nome de uma constante em letras maiúsculas
+e se tiver nome composto deve ser separado por _
+/*
+
+/** Modificador final
+Define que um determinado valor não será modificado por
+ninguém;
+Uma vez definido um valor com um modificador final
+ele será uma constante;
+Sempre escrito em letra minúscula;
+/*
