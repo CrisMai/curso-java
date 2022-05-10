e4eb4adf76a8f60a12d781b840de331a3c69e7e0
Cris Mai <crismai.dev@gmail.com>
2022-05-10 14:30:30 -0300

    Primeiro HelloWorld em java

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/**Programa que imprime texto na tela.
+ * @author Cris Mai
+ */
+
+public class HelloWorld {
+    /*Método principal da classe */
+
+    public static void main (String[] args ) {
+        System.out.println("\t Hello World"); // Caracteres de escape: \t, \n, \", \\
+        System.out.println("Java \n SE");
+
+    } //Fim do método
+
+} // Fim da classe
