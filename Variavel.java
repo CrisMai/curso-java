8536a2d63325a526a1b116850178d64951c3fa2c
Cris Mai <crismai.dev@gmail.com>
2022-05-10 14:54:29 -0300

    Variáveis e sintaxe

---
+++ Variavel.java
@@ -0,0 +1,25 @@
+// Apresentar os conceitos de variável
+
+
+public class Variavel {
+	
+	public static void main (String [] args) {
+		
+		String nome = "Cris Mai";
+		int idade = 36;
+		boolean casada = true;
+		
+		System.out.println(nome);
+		System.out.println(idade);
+		
+	}
+	
+}
+
+/**
+As variáveis sempre começam com letra minúscula;
+Devem ter nomes significativos;
+Não podem ser criados nomes de variáveis que sejam palavras
+chaves da linguagem Java;
+*/
+	
