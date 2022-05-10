e81baf3c60348018437aee9d3dc69d38449a7bb7
Cris Mai <crismai.dev@gmail.com>
2022-05-10 16:30:23 -0300

    Classes wrapper

---
+++ Wrapper.java
@@ -0,0 +1,26 @@
+// Apresentar as classes Wrapper
+
+
+public class Wrapper {
+	
+	public static void main (String [] args) {
+		
+		Integer idade = new Integer (36);
+		Double preco = new Double ("12.45");
+				
+	}
+	
+}
+
+/** Classes Wrapper
+São empacotadores do tipo primitivo, representam
+um invólucro desse tipo;
+Oferecem alguns recursos de conversão, por exemplo, 
+converter um tipo texto para tipo double;
+
+Wrappers:
+Integer Byte Short
+Long Float Double
+Character Void Boolean
+
+/*
