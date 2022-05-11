5fb6754edd537cd978033f6ef3ecd10e6044b0da
Cris Mai <crismai.dev@gmail.com>
2022-05-11 14:06:01 -0300

    Operadores

---
+++ Operador.java
@@ -0,0 +1,26 @@
+
+public class Operador {
+	
+	public static void main (String[] args) {
+		
+		//int x = 9 + 4;
+		//String x = "9" + "4";
+		//System.out.println(x);
+		
+		double y = 7 - 4 + 3 * 2;
+		System.out.println(y);
+	}
+
+}
+
+/** Tipos de Operador
+-2 operador unário;
+2 + 3 operador binário;
+true ? "sim" : "não" operador ternário;
+
+*/
+
+/** Precedência dos operadores
+Multiplicação e divisão têm precedência sobre
+as operações de soma, subtração;
+*/
