119547de4c2240344c7dfe10b665be77b8614396
Cris Mai <crismai.dev@gmail.com>
2022-05-11 15:35:53 -0300

    Opreadores lógicos

---
+++ OperLogico.java
@@ -0,0 +1,21 @@
+
+public class OperLogico {
+	
+	public static void main (String[] args) {
+		
+		int x = 6;
+		
+		System.out.println((x >= 1) && (x <= 10));
+		System.out.println((x >= 1) || (x <= 5));	
+		System.out.println( !(x >= 1);
+		
+	}
+
+}
+
+/** Operadores lógicos
+&& and (e)
+|| or (ou)
+! not (não)
+
+*/
