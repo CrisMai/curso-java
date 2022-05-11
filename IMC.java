1fecdd9a8f28ef99e942f342dd50fc61c0e263de
Cris Mai <crismai.dev@gmail.com>
2022-05-11 16:42:22 -0300

    Cálculo do IMC

---
+++ IMC.java
@@ -0,0 +1,23 @@
+// Calcular o IMC
+
+import javax.swing.JOptionPane;
+
+public class IMC {
+	
+	public static void main (String[] args) {
+		
+		String peso = JOptionPane.showInputDialog("Qual o seu peso?");
+		String altura = JOptionPane.showInputDialog("Qual a sua altura?");
+		
+		double pesoEmQuilogramas = Double.parseDouble(peso);
+		double alturaEmMetros = Double.parseDouble(altura);
+		double imc = pesoEmQuilogramas / (alturaEmMetros * alturaEmMetros);
+		
+		String msg = (imc >= 18.9 && imc <= 24.9) ? "IMC adequado" : "Fora do IMC adequado";
+		msg = "IMC = " + imc + "\n" + msg;
+		
+		JOptionPane.showInputDialog(null, msg);
+			
+	}
+
+}
