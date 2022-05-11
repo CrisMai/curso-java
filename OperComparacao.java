c8b8750119cab1f28d5b623bfe6cd691e73bb11c
Cris Mai <crismai.dev@gmail.com>
2022-05-11 15:28:26 -0300

    Operadores de comparação

---
+++ OperComparacao.java
@@ -0,0 +1,22 @@
+
+public class OperComparacao {
+	
+	public static void main (String[] args) {
+		
+		int x = 6;
+		
+		System.out.println(x == 6);
+		
+		
+	}
+
+}
+
+/** Operadores de comparação
+== igual
+!= diferente
+> maior
+< menor
+>= maior ou igual
+<= menor oi igual
+instanceof compara tipos
