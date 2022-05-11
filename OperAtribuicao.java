e6dee8e1504f8e4e41e4eba683f2c32be233903d
Cris Mai <crismai.dev@gmail.com>
2022-05-11 15:58:24 -0300

    Operadores de atribuição

---
+++ OperAtribuicao.java
@@ -0,0 +1,23 @@
+
+public class OperAtribuicao {
+	
+	public static void main (String[] args) {
+		
+		int x = 6;
+		x += 3;
+		
+			
+		System.out.println(x);
+		
+	}
+
+}
+
+/** Operadores de atribuição
++= adição
+-= subtração
+*= multiplicação
+/= divisão
+%= módulo
+
+*/
