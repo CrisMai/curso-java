0b984ffa1ae03ab1d57a332ece10c166dbbc2c62
Cris Mai <crismai.dev@gmail.com>
2022-05-11 13:40:30 -0300

    Entrada de dados com Scanner

---
+++ Entrada.java
@@ -0,0 +1,15 @@
+
+
+public class Entrada {
+	
+	public static void main (String[] args ) {
+		//Chamada do Programa
+		//System.out.println(args[0]);
+		
+		//Interagindo com o usuário
+		Scanner s = new Scanner (System.in);
+		System.out.println("Qual o seu Nome?");
+		String nome = s.nextLine();
+		System.out.println("Bem vindo " + nome);
+	}
+} 
