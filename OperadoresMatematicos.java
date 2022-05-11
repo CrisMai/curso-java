bfac5716088cbb70931c5a831fa664c8a723bb73
Cris Mai <crismai.dev@gmail.com>
2022-05-11 14:21:09 -0300

    Operadores matemáticos

---
+++ OperadoresMatematicos.java
@@ -0,0 +1,35 @@
+
+public class OperadoresMatematicos {
+	
+	public static void main (String[] args) {
+		
+		double x = 7 % 3;
+		System.out.println(x);
+		
+		int x = 6;
+		int y = ++x; //pré-incremento
+		
+		int x = 6;
+		int y = x++; //pós-incremento
+		
+		
+		System.out.println("x=" + x);
+		System.out.println("y=" + y);
+		
+	}
+
+}
+
+
+/** Operadores matemáticos
++ adição
+- subtração
+* multiplicação
+/ divisão
+% módulo
++ positivo
+- negativo
+++ incremento +1
+-- decremento -1
+
+*/
