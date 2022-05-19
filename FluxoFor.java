0a70f4e9d795040a3e25d558027a83b4bb3f6f84
Cris Mai <crismai.dev@gmail.com>
2022-05-19 15:56:17 -0300

    Estrutura For

---
+++ FluxoFor.java
@@ -0,0 +1,15 @@
+
+public class FluxoFor {
+	
+	public static void main (String[] args) {
+		
+		String texto = "";
+		
+		for (int i=0; i<3; i++) {
+			texto += i + ",";
+		}
+		
+		System.out.println (texto);
+		
+	}
+}
