5916bef8ddff1d17fd866fc001b857a2591fe3e9
Cris Mai <crismai.dev@gmail.com>
2022-05-19 15:33:29 -0300

    Switch

---
+++ FluxoSwitch.java
@@ -0,0 +1,21 @@
+
+public class FluxoSwitch {
+	
+	public static void main (String[] args) {
+		
+		char sexo = 'M';
+		
+		switch (sexo) {
+			case 'M':
+				System.out.println ("Macho");
+				break;
+			case 'F':
+				System.out.println ("Femea");
+				break;
+			default:
+				System.out.println ("Outro");
+				
+		}
+		
+	}
+}
