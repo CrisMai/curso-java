d8452e9228f54694692014f1e27b0e087578682b
Cris Mai <crismai.dev@gmail.com>
2022-05-19 15:20:17 -0300

    Instrução if else

---
+++ Fluxo.java
@@ -0,0 +1,19 @@
+
+
+public class Fluxo {
+	
+	public static void main (String[] args) {
+		
+		int idade = 31;
+		if (idade <= 11) {
+			System.out.println ("Crianca");
+		} else if (idade > 11 && idade <= 18) {
+			System.out.println ("Adolescente");
+		} else if (idade > 18 && idade <= 60) {
+			System.out.println ("Adulto");
+		} else {
+			System.out.println ("Idoso");
+		}
+	
+	}
+}
