f581ae9920a859bfc6a8e6075f735a96443f8287
Cris Mai <crismai.dev@gmail.com>
2022-05-23 15:11:52 -0300

    Pacotes package

---
+++ br/com/cris/logica/CachorroTestExterno.java
@@ -0,0 +1,13 @@
+package br.com.cris.logica;
+
+import br.com.cris.poo.Cachorro;
+
+public class CachorroTestExterno {
+
+	public static void main(String[] args) {
+		
+		Cachorro c = new Cachorro();
+
+	}
+
+}
