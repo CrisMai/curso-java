44b341345df86a1a7c3b7b1d8b2bebd37f199d39
Cris Mai <crismai.dev@gmail.com>
2022-05-24 14:29:13 -0300

    Métodos com retorno

---
+++ br/com/cris/poo/Matematica.java
@@ -0,0 +1,20 @@
+package br.com.cris.poo;
+
+public class Matematica {
+	
+	int maior(int um, int dois) { //método com retorno e com 2 parâmetros
+		if(um > dois) {
+			return um;
+		}
+		else {
+			return dois;
+		}
+		
+	}
+	
+	double soma(double um, double dois) {
+		double s = um + dois;
+		return s;
+	}
+
+}
---
+++ br/com/cris/poo/MatematicaTest.java
@@ -0,0 +1,16 @@
+package br.com.cris.poo;
+
+public class MatematicaTest {
+
+	public static void main(String[] args) {
+		
+		Matematica m = new Matematica();
+		int ma = m.maior(50, 20);
+		System.out.println(ma);
+		
+		double so = m.soma(20, 20);
+		System.out.println(so);
+
+	}
+		
+}
