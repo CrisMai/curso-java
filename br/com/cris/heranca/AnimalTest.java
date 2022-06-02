19ab215f5920529fdef817bd2afd39dcc84e322d
unknown <crismai.dev@gmail.com>
2022-06-02 16:43:26 -0300

    Herança

---
+++ br/com/cris/heranca/AnimalTest.java
@@ -0,0 +1,33 @@
+package br.com.cris.heranca;
+
+public class AnimalTest {
+
+	public static void main(String[] args) {
+		
+		Cachorro toto = new Cachorro();
+		toto.comida = "Carne";
+		toto.dormir();
+		
+		Galinha carijo = new Galinha();
+		carijo.fazerBarulho();
+
+	}
+
+}
+
+
+
+/*
+ * Como criar uma HERANÇA?
+ * Procurar os objetos que possuam atributos e comportamentos 
+ * em comum.
+ * 
+ * Quando tiver um comportamento que deve ser compartilhado
+ * entre várias classes do mesmo tipo -> use a Herança.
+ * 
+ * Quando uma classe é uma subclasse de outra é pq ela
+está herdando atributos e métodos da superclasse, ou seja,
+pode utilizar esses atributos como se fossem aplicados
+dentro dela.
+
+ */
---
+++ br/com/cris/heranca/Cachorro.java
@@ -0,0 +1,19 @@
+package br.com.cris.heranca;
+
+public class Cachorro extends Animal {
+
+}
+
+
+/*
+ * HERANÇA
+ * A Herança é um relacionamento É-UM.
+ * A Herança permite o reaproveitamento de códigos.
+ * A Herança permite criar superclasses que recebem
+ * esses métodos e atributos semelhantes.
+ * É com a palavra extends que é transformado uma classe 
+ * em subclasse de uma outra classe.
+ 
+ */
+
+
