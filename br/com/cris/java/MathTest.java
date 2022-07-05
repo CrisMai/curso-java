7a245300dca9a8b683ca5c9b7eedc8fa59dd6f78
unknown <crismai.dev@gmail.com>
2022-07-05 14:15:05 -0300

    Math

---
+++ br/com/cris/java/MathTest.java
@@ -0,0 +1,17 @@
+package br.com.cris.java;
+
+public class MathTest {
+	
+	public static void main(String[] Args) {
+		
+		System.out.println(Math.sin(10)); // Seno
+		System.out.println(Math.cos(10)); // Cosseno
+		System.out.println(Math.tan(10)); // Tangente
+		System.out.println(Math.pow(4, 2)); // Potência
+		System.out.println(Math.sqrt(25)); // Raiz quadrada
+		System.out.println(Math.cbrt(16)); // Raiz cúbica
+		System.out.println(Math.hypot(6, 8)); // Hypotenusa
+		
+	}
+
+}
