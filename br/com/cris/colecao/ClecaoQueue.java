703d9dc8dc7db73120cce5a9dff77e5561ec951a
unknown <crismai.dev@gmail.com>
2022-07-19 14:42:02 -0300

    Collection Queue

---
+++ br/com/cris/colecao/ClecaoQueue.java
@@ -0,0 +1,38 @@
+package br.com.cris.colecao;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ClecaoQueue {
+	
+	public static void main(String[] args) {
+		
+		// FIFO => O primeiro a entrar é o primeiro a sair
+		Queue<String> fila = new LinkedList<>();
+		fila.add("Ana");
+		fila.add("Cris");
+		fila.add("Eliz");
+		fila.offer("Carla"); // Esses dois métodos adicionam elementos ao final da fila.
+		
+		System.out.println(fila);
+		System.out.println(fila.peek()); // Método que tras o próximo elemento da fila.
+		System.out.println(fila.poll()); // Método que remove o elemento do início da fila.
+		System.out.println(fila);
+		
+		
+		/* Outros métodos disponíveis em LinkedList */
+		LinkedList<String> f = (LinkedList<String>) fila;
+		f.addFirst("Caio"); // Método que adiciona um elemento ao início da fila.
+		f.addLast("Rui"); // Adiciona no final da fila.
+		System.out.println(f);
+		
+		f.pollFirst();
+		f.pollLast();
+		System.out.println(f);
+	}
+
+}
+
+/* Collection Queue representam filas de espera.
+Existem duas classes que implementam as interfaces Queue:
+LinkedList e PriorityQueue. */ 
